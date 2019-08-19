11d3eb4abfb0567bb51899d26b7f5c70af54f09b
Shahar <ravidar10@gmail.com>
2019-08-19 20:47:37 +0300

    added profile and security tokens for users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     all: (req, res) => {
@@ -11,5 +12,27 @@ module.exports = {
         user.save()
             .then(user => res.status(201).json(user))
             .catch(err => res.status(400).json(err));
+    },
+    login: (req, res) => {
+        User.findOne({
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then (user => {
+            if(user) {
+                const token = jwt.sign({id: user._id}, 'ABCD');
+                res.json({token});
+            } else {
+                res.status(403).json({});
+            }
+        })
+        .catch(err => res.status(500).json(err));
+    },
+    me: (req, res) => {
+        const user = jwt.verify(req.headers.authorization,'ABCD');
+        console.log(user);
+        User.findOne({_id: user.id})
+        .then(user => res.json(user))
+        .catch(err => res.send(err));
     }
 }
